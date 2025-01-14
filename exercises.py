472df06aabe1487267608c9af2a8a0b37ba67bd4
Uma <umi_lumi@yahoo.com>
2025-01-14 15:42:16 +0100

    Question 2b

--- exercises.py
+++ exercises.py
@@ -22,9 +22,33 @@ for key in seattle:
 total_monthly_precipitation = list(monthly_precipitation.values())
 total_yearly_precipitation = sum(total_monthly_precipitation)
 
+relative_monthly_precipitation = {}
+total_yearly_precipitation = sum(monthly_precipitation.values())
+
+date = key['date']  
+month = date.split('-')[1]
+percentation = key['value']  
+for month, precipitation in monthly_precipitation.items():
+    percentage = ( int((precipitation)) / int(total_yearly_precipitation)) * 100
+    relative_monthly_precipitation[month] = percentage
+
+
+for month, percentage in relative_monthly_precipitation.items():
+
+
+    stats = {
+    'monthly_precipitation': monthly_precipitation,
+    'total_yearly_precipitation': total_yearly_precipitation,
+    'relative_montly_precipitation': relative_monthly_precipitation
+    }
+
+with open ('results.json', 'w') as file:
+    json.dump(stats, file, indent=4)
+
 stats = {
     'monthly_precipitation': monthly_precipitation,
     'total_yearly_precipitation': total_yearly_precipitation,
+    'relative_montly_precipitation': relative_monthly_precipitation
     }
 
 with open ('results.json', 'w') as file:
